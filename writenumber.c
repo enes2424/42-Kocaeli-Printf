5a920f3470cc77e16d652735f409a27c9d53c121
Enes Mahmut Ates <eates@k2m14s05.42kocaeli.com.tr>
2024-11-08 14:24:27 +0300

    printf

--- writenumber.c
+++ writenumber.c
@@ -14,7 +14,7 @@
 
 int	writeint(int n, t_printf *x)
 {
-	int		arr[10];
+	char	arr[10];
 	int		i;
 
 	i = 0;
@@ -30,11 +30,11 @@ int	writeint(int n, t_printf *x)
 	}
 	while (n)
 	{
-		arr[i++] = n % 10;
+		arr[i++] = DECIMAL[n % 10];
 		n /= 10;
 	}
 	while (i--)
-		if (writechar(DECIMAL[arr[i]], x) == -1)
+		if (writechar(arr[i], x) == -1)
 			return (-1);
 	return (1);
 }
@@ -49,42 +49,42 @@ int	writeuint(unsigned long n, t_printf *x)
 		return (writechar('0', x));
 	while (n)
 	{
-		arr[i++] = n % 10;
+		arr[i++] = DECIMAL[n % 10];
 		n /= 10;
 	}
 	while (i--)
-		if (writechar(DECIMAL[arr[i]], x) == -1)
+		if (writechar(arr[i], x) == -1)
 			return (-1);
 	return (1);
 }
 
 int	writehex(unsigned long n, t_printf *x)
 {
-	int		arr[16];
+	char	arr[16];
 	int		i;
 	char	*hex;
 
 	i = 0;
 	if (!n)
-		arr[i++] = 0;
-	while (n)
-	{
-		arr[i++] = n % 16;
-		n /= 16;
-	}
+		arr[i++] = '0';
 	if (x->f == 'x')
 		hex = HEXALOW;
 	else
 		hex = HEXAUP;
+	while (n)
+	{
+		arr[i++] = hex[n % 16];
+		n /= 16;
+	}
 	while (i--)
-		if (writechar(hex[arr[i]], x) == -1)
+		if (writechar(arr[i], x) == -1)
 			return (-1);
 	return (1);
 }
 
 int	writepoint(void *n, t_printf *x)
 {
-	int					arr[32];
+	char				arr[32];
 	int					i;
 	unsigned long long	nb;
 
@@ -96,11 +96,11 @@ int	writepoint(void *n, t_printf *x)
 		return (writechar('0', x));
 	while (nb)
 	{
-		arr[i++] = nb % 16;
+		arr[i++] = HEXALOW[nb % 16];
 		nb /= 16;
 	}
 	while (i--)
-		if (writechar(HEXALOW[arr[i]], x) == -1)
+		if (writechar(arr[i], x) == -1)
 			return (-1);
 	return (1);
 }
